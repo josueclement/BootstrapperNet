c1ce7b1e78906b34738da452acb0eaaa1b725ec7
Josué Clément <mod6991@gmail.com>
2023-02-13 20:47:16 +0100

    Import bootstrapper classes from old project

---
+++ BootstrapperNet/Bootstrapper.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace BootstrapperNet
+{
+    /// <summary>
+    /// Bootstrapper for .NET applications
+    /// </summary>
+    public abstract class Bootstrapper
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructor for <see cref="Bootstrapper"/>
+        /// </summary>
+        public Bootstrapper()
+        {
+            Current = this;
+            ServiceCollection = new ServiceCollection();
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Occurs when the bootstrapper is starting
+        /// </summary>
+        public event EventHandler? Starting;
+
+        /// <summary>
+        /// Occurs when the bootstrapper started everything
+        /// </summary>
+        public event EventHandler? Started;
+
+        /// <summary>
+        /// Occurs when an unhandled exception is thrown
+        /// </summary>
+        public event EventHandler<Exception>? UnhandledException;
+
+        /// <summary>
+        /// Raises <see cref="Starting"/> event
+        /// </summary>
+        protected void RaiseStartingEvent()
+        {
+            Starting?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Raises <see cref="Started"/> event
+        /// </summary>
+        protected void RaiseStartedEvent()
+        {
+            Started?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Raises <see cref="UnhandledException"/> event
+        /// </summary>
+        /// <param name="ex">Unhandled exception</param>
+        protected void RaiseUnhandledExceptionEvent(Exception ex)
+        {
+            UnhandledException?.Invoke(this, ex);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Current bootstrapper
+        /// </summary>
+        public static Bootstrapper? Current { get; protected set; }
+
+        /// <summary>
+        /// Services collection
+        /// </summary>
+        public ServiceCollection ServiceCollection { get; protected set; }
+
+        /// <summary>
+        /// Services provider
+        /// </summary>
+        public ServiceProvider? ServiceProvider { get; protected set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Runs the bootstrapper
+        /// </summary>
+        public virtual void Run()
+        {
+            RaiseStartingEvent();
+            ConfigureServices();
+            RegisterUnhandledExceptions();
+            RaiseStartedEvent();
+        }
+
+        /// <summary>
+        /// Terminates the bootstrapper
+        /// </summary>
+        public virtual void Shutdown()
+        {
+            UnregisterUnhandledExceptions();
+
+            ServiceCollection.Clear();
+            ServiceProvider?.Dispose();
+        }
+
+        #endregion
+
+        #region Dependency injection methods
+
+        /// <summary>
+        /// Configure services<br />
+        /// Override this method to add custom services
+        /// </summary>
+        /// <param name="services">Services</param>
+        protected virtual void ConfigureServices(IServiceCollection services)
+        { }
+
+        /// <summary>
+        /// Configures services and build service provider
+        /// </summary>
+        protected void ConfigureServices()
+        {
+            ConfigureServices(ServiceCollection);
+            ServiceProvider = ServiceCollection.BuildServiceProvider();
+        }
+
+        #endregion
+
+        #region Unhandled exceptions methods
+
+        /// <summary>
+        /// Registers unhandled exceptions<br />
+        /// Override this method to handle other unhandled exceptions
+        /// </summary>
+        protected virtual void RegisterUnhandledExceptions()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        }
+
+        /// <summary>
+        /// Unregisters unhandled exceptions<br />
+        /// Override this method to unhandle other unhandled exceptions
+        /// </summary>
+        protected virtual void UnregisterUnhandledExceptions()
+        {
+            AppDomain.CurrentDomain.UnhandledException -= CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException -= TaskScheduler_UnobservedTaskException;
+        }
+
+        /// <summary>
+        /// Method called when <see cref="AppDomain.UnhandledException"/> is raised
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Unhandled exception args</param>
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+                RaiseUnhandledExceptionEvent(ex);
+        }
+
+        /// <summary>
+        /// Method called when <see cref="TaskScheduler.UnobservedTaskException"/> is raised
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Unobserved task exception args</param>
+        private void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            e.SetObserved();
+            RaiseUnhandledExceptionEvent(e.Exception);
+        }
+
+        #endregion
+    }
+}
---
+++ BootstrapperNet/WpfBootstrapper.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace BootstrapperNet
+{
+    /// <summary>
+    /// Bootstrapper for Wpf applications
+    /// </summary>
+    public abstract class WpfBootstrapper : Bootstrapper
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets wether the splash screen is enabled
+        /// </summary>
+        public virtual bool IsSplashScreenEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the splash screen duration
+        /// </summary>
+        public virtual TimeSpan SplashScreenDuration { get; set; } = TimeSpan.FromSeconds(2);
+
+        /// <summary>
+        /// Splash screen window
+        /// </summary>
+        public Window? SplashScreenWindow { get; protected set; }
+
+        /// <summary>
+        /// Main window
+        /// </summary>
+        public Window? MainWindow { get; protected set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <inheritdoc/>
+        public override void Run()
+        {
+            RaiseStartingEvent();
+            ConfigureServices();
+            RegisterUnhandledExceptions();
+            ShowSplashScreenAndWindow();
+            RaiseStartedEvent();
+        }
+
+        /// <inheritdoc/>
+        public override void Shutdown()
+        {
+            base.Shutdown();
+
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #region Unhandled exceptions methods
+
+        /// <inheritdoc/>
+        protected override void RegisterUnhandledExceptions()
+        {
+            base.RegisterUnhandledExceptions();
+
+            Application.Current.DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
+
+        /// <inheritdoc/>
+        protected override void UnregisterUnhandledExceptions()
+        {
+            base.UnregisterUnhandledExceptions();
+
+            Application.Current.DispatcherUnhandledException -= OnDispatcherUnhandledException;
+        }
+
+        /// <summary>
+        /// Method called when <see cref="Application.DispatcherUnhandledException"/> is raised
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Dispatcher unhandled exception args</param>
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            RaiseUnhandledExceptionEvent(e.Exception);
+        }
+
+        #endregion
+
+        #region Splash screen and Main window
+
+        /// <summary>
+        /// Create a new splash screen window<br />
+        /// Override this method to create your own splash screen window
+        /// </summary>
+        /// <returns>Splash screen window</returns>
+        protected virtual Window CreateSplashScreenWindow() => new Window();
+
+        /// <summary>
+        /// Create a new main window<br />
+        /// Override this method to create your own main window
+        /// </summary>
+        /// <returns>Main window</returns>
+        protected abstract Window CreateMainWindow();
+
+        /// <summary>
+        /// Show splash screen and main window
+        /// </summary>
+        protected virtual void ShowSplashScreenAndWindow()
+        {
+            MainWindow = CreateMainWindow();
+
+            if (IsSplashScreenEnabled)
+            {
+                SplashScreenWindow = CreateSplashScreenWindow();
+                SplashScreenWindow.Show();
+
+                DateTime splashScreenEnd = DateTime.Now + SplashScreenDuration;
+
+                while (DateTime.Now < splashScreenEnd)
+                {
+                    Thread.Sleep(200);
+                }
+
+                SplashScreenWindow.Close();
+            }
+
+            MainWindow.Show();
+            MainWindow.Closing += OnMainWindowClosing;
+        }
+
+        /// <summary>
+        /// Occurs when the main window is closing
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Cancel event args</param>
+        protected virtual void OnMainWindowClosing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MainWindow != null)
+                MainWindow.Closing -= OnMainWindowClosing;
+
+            Shutdown();
+        }
+
+        #endregion
+    }
+}
